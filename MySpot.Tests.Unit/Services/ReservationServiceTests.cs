a8869120a65fce5d705afcdf7f7fee551795d690
Bartosz Cłapiński <bartosz.clapinski@gmail.com>
2023-05-10 14:58:19 +0200

    Service Test Added. Not fininshed - from trip

---
+++ MySpot.Tests.Unit/Services/ReservationServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MySpot.Api.Commands;
+using MySpot.Api.Services;
+using MySpot.Api.ValueObjects;
+using Shouldly;
+using Xunit;
+
+namespace MySpot.Tests.Unit.Services;
+
+public class ReservationServiceTests
+{
+    #region Arrange
+
+    private readonly ReservationsService _reservationsService;
+    
+    public ReservationServiceTests()
+    {
+        _reservationsService = new ReservationsService();
+    }
+
+    #endregion
+    
+    [Fact]
+    public void given_reservation_for_not_taken_date_create_should_succeed()
+    {
+        //  Arrange
+        var command = new CreateReservation(
+            Guid.Parse("00000000-0000-0000-0000-000000000001"),
+            Guid.NewGuid(),
+            "John Doe",
+            "XYZ123",
+            new Date(DateTime.UtcNow.AddMinutes(5)));
+        
+        //  Act
+        var reservationId = _reservationsService.Create(command);
+        
+        //  Assert
+        reservationId.ShouldNotBeNull();
+        reservationId.Value.ShouldBe<Guid>(command.ReservationId);
+    }
+}
